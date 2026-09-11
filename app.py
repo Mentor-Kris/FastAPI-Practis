af300712cf4d88cef2d0eae9d58d828c153f66e2
Kris <bhanderikrishc204@gmail.com>
2026-09-11 14:18:24 +0530

    nested data

--- app.py
+++ app.py
@@ -14,15 +14,33 @@
 #         "Limit":limit,
 #         "Active":active
 #                 } 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# app=FastAPI()
+# class User(BaseModel):
+#     name:str
+#     age:int
+# @app.post("/user")
+# def creat_user(user:User):
+#     return {
+#         "message":"User created",
+#         "data":user 
+#     }    
 from fastapi import FastAPI
 from pydantic import BaseModel
 app=FastAPI()
-class User(BaseModel):
+class Address(BaseModel):
+    state:str
+    city:str
+    pincode:int 
+class Student(BaseModel):
     name:str
     age:int
+    college:str
+    address:Address
 @app.post("/user")
-def creat_user(user:User):
+def creat_user(user:Student):
     return {
-        "message":"User created",
-        "data":user 
-    }    
+        "message":"User created sucessfully",
+        "User":user 
+    }        
