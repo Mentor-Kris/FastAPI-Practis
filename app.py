30924fddf267c9b8f65e653a7fba6256f9336bbd
Kris <bhanderikrishc204@gmail.com>
2026-09-11 16:15:58 +0530

    path+query+body combo

--- app.py
+++ app.py
@@ -1,47 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 app=FastAPI()
-students=[]
-class Student(BaseModel):
-    id:int
+users=[]
+class User(BaseModel):
     name:str
-    age:int
-    college:str
-    city:str 
-@app.post("/student/{student_id}")
-def creat_student(student_id:int,student:Student):
-    students.append(student)
+    age:int 
+@app.post("/users")
+def create_user(user:User):
+    users.append(user)
     return {
-        "message":"Student created",
-        "student":students
-    }    
-@app.get("/student")
-def get_student():
-    return {
-        "student":students
+        "message":"user created",
+        "data":user 
     }
-@app.get("/student/{student_id}")
-def get_student(student_id:int):
-    for student in students:
-        if student.id==student_id:
-            return student 
-@app.put("/student/{student_id}")
-def update_student(student_id:int,update_student:Student):
-    for index,student in enumerate(students):
-        if student.id==student_id:
-            students[index]=update_student        
+@app.put("/users/{user_id}")
+def update_user(user_id:int,user:User,notify:bool):
+    if user_id<len(users):
+        users[user_id]=user
         return {
-            "message":"student update sucessfully",
-            "student":update_student
-        }    
-@app.delete("/student/{student_id}")
-def student_delete(student_id:int):
-    for index ,student in enumerate(students):
-        students.pop(index)
+            "message":"user updated",
+            "notify":notify,
+            "data":user
+        }
     return {
-        "message":"student deleted sucessfully"
-        
+        "error":"User not found"
     }
-    return {
-        "error":"student was not found"
-    }    
