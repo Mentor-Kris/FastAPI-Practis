843c044bb5d81fadaf5857542b965e6125f8735d
Kris <bhanderikrishc204@gmail.com>
2026-09-11 13:58:22 +0530

    query perameter

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+# from fastapi import FastAPI
+# app=FastAPI()
+# @app.get("/")
+# def get():
+#     return {
+#         "message":"Hello FastAPI"
+#     }
 from fastapi import FastAPI
 app=FastAPI()
-@app.get("/")
-def get():
+@app.get("/user")
+def get_user(limit:int,active:bool):
     return {
-        "message":"Hello FastAPI"
-    }
+        "message":"query parameter",
+        "Limit":limit,
+        "Active":active
+                } 
