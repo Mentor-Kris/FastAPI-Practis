9b745daed3a0fbb1f65a352a024b854086f1f001
Kris <bhanderikrishc204@gmail.com>
2026-09-11 15:41:58 +0530

    crud practis 2

--- app.py
+++ app.py
@@ -1,91 +1,47 @@
-# from fastapi import FastAPI
-# app=FastAPI()
-# @app.get("/")
-# def get():
-#     return {
-#         "message":"Hello FastAPI"
-#     }
-# from fastapi import FastAPI
-# app=FastAPI()
-# @app.get("/user")
-# def get_user(limit:int,active:bool):
-#     return {
-#         "message":"query parameter",
-#         "Limit":limit,
-#         "Active":active
-#                 } 
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# app=FastAPI()
-# class User(BaseModel):
-#     name:str
-#     age:int
-# @app.post("/user")
-# def creat_user(user:User):
-#     return {
-#         "message":"User created",
-#         "data":user 
-#     }    
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# app=FastAPI()
-# class Address(BaseModel):
-#     state:str
-#     city:str
-#     pincode:int 
-# class Student(BaseModel):
-#     name:str
-#     age:int
-#     college:str
-#     address:Address
-# @app.post("/user")
-# def creat_user(user:Student):
-#     return {
-#         "message":"User created sucessfully",
-#         "User":user 
-#     }  
 from fastapi import FastAPI
 from pydantic import BaseModel
 app=FastAPI()
-Book=[]
-class Books(BaseModel):
+students=[]
+class Student(BaseModel):
     id:int
     name:str
-    author:str
-@app.post("/book/{book_id}")
-def get_book(book_id:int,book:Books):
-    Book.append(book)
+    age:int
+    college:str
+    city:str 
+@app.post("/student/{student_id}")
+def creat_student(student_id:int,student:Student):
+    students.append(student)
     return {
-        "message":"book created sucessfully",
-        "Book":Book 
-    }
-@app.get("/book")
-def get_book():
+        "message":"Student created",
+        "student":students
+    }    
+@app.get("/student")
+def get_student():
     return {
-           "Book":Book 
+        "student":students
     }
-@app.get("/book/{book_id}")
-def get_book(book_id:int):
-    for book in Book:
-        if book.id==book_id:
-            return book 
-@app.put("/book/{book_id}")
-def update_book(book_id:int,update_book:Books):
-    for index,book in enumerate(Book):
-        if book.id==book_id:
-            Book[index]=update_book
+@app.get("/student/{student_id}")
+def get_student(student_id:int):
+    for student in students:
+        if student.id==student_id:
+            return student 
+@app.put("/student/{student_id}")
+def update_student(student_id:int,update_student:Student):
+    for index,student in enumerate(students):
+        if student.id==student_id:
+            students[index]=update_student        
         return {
-            "message":"data updated",
-            "data":update_book 
-        }            
-@app.delete("/book/{book_id}")
-def book_delete(book_id:int):
-    for index,book in enumerate(Book):
-        if book.id==book_id:
-            Book.pop(index)
+            "message":"student update sucessfully",
+            "student":update_student
+        }    
+@app.delete("/student/{student_id}")
+def student_delete(student_id:int):
+    for index ,student in enumerate(students):
+        students.pop(index)
     return {
-        "message":"book delete sucessfully"
+        "message":"student deleted sucessfully"
+        
     }
     return {
-        "error":"book not found"
-    }        
+        "error":"student was not found"
+    }    
